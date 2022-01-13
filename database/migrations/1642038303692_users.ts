aa680365b066452c886c4d445ffe893793cc14f0
Bruno Puzoni <bpuzoni@gmail.com>
2022-01-12 23:30:36 -0300

    feat: define CreateUsers migration

---
+++ database/migrations/1642038303692_users.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.raw('gen_random_uuid()'))
+      table.string('email').unique().notNullable()
+      table.string('password').notNullable()
+
+      table.string('name').notNullable()
+
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
