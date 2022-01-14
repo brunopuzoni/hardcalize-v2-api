7697a97f879f76034d53c7787db0df5b1136f2e4
Bruno Puzoni <bpuzoni@gmail.com>
2022-01-13 23:15:38 -0300

    chore: setup hash driver

--- env.ts
+++ env.ts
@@ -20,6 +20,7 @@ export default Env.rules({
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
+  HASH_DRIVER: Env.schema.enum(['argon', 'bcrypt'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
   DB_CONNECTION: Env.schema.string(),
   PG_HOST: Env.schema.string({ format: 'host' }),
